38ec65049add762c24b9ed66b9529c99270d2207
lailah smith <117845543+basquiat13@users.noreply.github.com>
2024-12-04 19:41:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -77,7 +77,13 @@ section {
   width: 80%;
 }
 
-/* Dark/Light Mode */
+const toggleButton = document.getElementById("toggle-theme");
+
+// Listen for a click on the button
+toggleButton.addEventListener("click", function() {
+    // Toggle the "dark-mode" class on the body element
+    document.body.classList.toggle("dark-mode");
+});
 body.dark-mode {
   background-color: var(--dark-bg);
   color: var(--light-bg);
